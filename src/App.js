afc0c8cc1bf78db12e6d47a49dfa69b734514fe7
aliensmart <kaoua17@gmail.com>
2020-06-04 11:29:05 -0400

    need to work more on the droppable container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import { Draggable } from '@syncfusion/ej2-base';
+import { Draggable, Droppable } from '@syncfusion/ej2-base';
 
 function App() {
+  let droppable = ()=>{ new Droppable(document.getElementById('droppable'), {
+    drop: (e) => {
+        e.droppedElement.querySelector('#drag').textContent = 'Dropped';
+    }
+  });
+}
   useEffect(()=>{
-    let draggable = new Draggable(document.getElementById('element'), { clone: false });
-}, [])
+    let draggable = new Draggable(document.getElementById('draggable'), { clone: false });
+    droppable()
+        
+}, [droppable])
 
   return (
     <div className="App" id='container'>
-        <div id='element'><p>Draggable Element</p></div>
+        {/* <div id='element'><p>Draggable Element</p></div> */}
+        <div id='droppable'><p>Droppable Target </p></div>
+        <div id='draggable'><p id='drag'>Draggable Element </p></div>
+        
     </div>
   );
 }
