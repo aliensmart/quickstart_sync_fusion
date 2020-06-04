09ce115e61a113400ef7095415e30fbff6f8df63
aliensmart <kaoua17@gmail.com>
2020-06-04 11:20:14 -0400

    changed componentDidMount into useeffect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import { Animation } from '@syncfusion/ej2-base';
 import './App.css';
+import { Draggable } from '@syncfusion/ej2-base';
 
 function App() {
-  let animation = new Animation({ name: 'FadeIn', duration: 5000 });
-    animation.animate(this.element1, { name: 'FadeOut' });
-    animation.animate(this.element2, { name: 'ZoomOut' });
   useEffect(()=>{
-        
-    
+    let draggable = new Draggable(document.getElementById('element'), { clone: false });
 }, [])
 
   return (
-    <div className="App">
-        <div id="element1" ref={(ele) => { this.element1 = ele; }}></div>
-        <div id="element2" ref={(ele) => { this.element2 = ele; }}></div>
-
+    <div className="App" id='container'>
+        <div id='element'><p>Draggable Element</p></div>
     </div>
   );
 }
