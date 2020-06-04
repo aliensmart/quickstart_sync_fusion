9f7b48937ff18791ee81521bcf1f064a22374c4b
aliensmart <kaoua17@gmail.com>
2020-06-04 09:32:09 -0400

    added icons and style in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import './App.css';
 
 function App() {
+  var iconList = ['e-search', 'e-upload', 'e-font'];
+        const listItems = iconList.map((icon, index) =>
+            <li><span className={`e-icons ${icon}`} key={index}></span></li>
+            );
+            var icons = `
+            .e-search:before {
+                content:'\\e993';
+            }
+            .e-upload:before {
+                content: '\\e725';
+            }
+            .e-font:before {
+                content: '\\e34c';
+            }
+            `
   return (
     <div className="App">
         <ButtonComponent type="primary">Button</ButtonComponent>
+        <style>{icons}</style>
+                <div className="icons">
+                    <ul>
+                        {listItems}
+                    </ul>
+                </div>
+
     </div>
   );
 }
